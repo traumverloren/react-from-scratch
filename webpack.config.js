6535bfe81453d90e04e59cf9fa80a36d6f50e6cf
Stephanie Nemeth <traumverloren@gmail.com>
2017-06-13 13:04:42 +0200

    initial react setup, webpack, babel, webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            filename: 'index.html',
+            template: __dirname + '/src/index.html',
+        })
+    ]
+};
