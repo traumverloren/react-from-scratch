bdfeaa71993d3aeddcabb31d9946d84b41284026
Stephanie Nemeth <traumverloren@gmail.com>
2017-06-13 13:30:58 +0200

    tweak webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,10 +15,16 @@ module.exports = {
     ]
   },
   plugins: [
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            filename: 'index.html',
-            template: __dirname + '/src/index.html',
-        })
-    ]
+    new webpack.HotModuleReplacementPlugin(), // Enable HMR
+    new HtmlWebpackPlugin({
+        inject: 'body',
+        filename: 'index.html',
+        template: __dirname + '/src/index.html',
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3000,
+    hot: true
+  }
 };
